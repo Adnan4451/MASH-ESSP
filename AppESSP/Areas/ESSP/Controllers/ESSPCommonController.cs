f997a75e0d3485dea3fa132437f0fc4d3924a6d4
unknown <madnan.cns@gmail.com>
2020-01-15 11:55:53 +0500

    first push

---
+++ AppESSP/Areas/ESSP/Controllers/ESSPCommonController.cs
@@ -0,0 +1,91 @@
+﻿using ESSPCORE.Common;
+using ESSPCORE.EF;
+using ESSPCORE.ESSP;
+using ESSPREPO.Generic;
+using ESSPSERVICE.ESSP;
+using ESSPSERVICE.Generic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppESSP.Areas.ESSP.Controllers
+{
+    public class ESSPCommonController : Controller
+    {
+        // GET: ESSP/ESSPJobCard
+        IJobCardESSPService JobCardESSPService;
+        ILeavesESSPService LeaveApplicationESSPService;
+        IEntityService<VEP_JobCardApplication> VEPJobCardApplicationService;
+        IEntityService<VAT_LeaveApplication> VATLeaveApplicationService;
+        IRepository<JobCardAppFlow> JobCardAppFlowService;
+        public List<string> ToasterMessages = new List<string>();
+        IDDService DDService;
+        public ESSPCommonController(IJobCardESSPService jobCardESSPService, IEntityService<JobCardApp> jobcardappService, IDDService dDService, IEntityService<JobCardDetail> jobcarddetailservice
+          , IEntityService<VEP_JobCardApplication> vEPJobCardApplicationService, IEntityService<VAT_LeaveApplication> vatLeaveApplicationService
+            , IRepository<JobCardAppFlow> jobCardAppFlowService, ILeavesESSPService leaveApplicationESSPService)
+        {
+            JobCardESSPService = jobCardESSPService;
+            VEPJobCardApplicationService = vEPJobCardApplicationService;
+            VATLeaveApplicationService = vatLeaveApplicationService;
+            JobCardAppFlowService = jobCardAppFlowService;
+            LeaveApplicationESSPService = leaveApplicationESSPService;
+            DDService = dDService;
+        }
+
+
+        [HttpGet]
+        public ActionResult CommentView(string FormName, int? PID)
+        {
+            VMESSPCommon vmEsspCommon = new VMESSPCommon();
+            vmEsspCommon.FormName = FormName;
+            vmEsspCommon.PID = PID;
+            if (vmEsspCommon.FormName == "JobCard")
+                ViewBag.StageID = new SelectList(DDService.GetJobCardStage().ToList().Where(aa => aa.PJobCardStageID != "P").ToList(), "PJobCardStageID", "JobCardStageName", "A");
+            else
+                ViewBag.StageID = new SelectList(DDService.GetLeaveStage().ToList().Where(aa => aa.PLeaveStageID != "P" && aa.PLeaveStageID != "A").OrderByDescending(aa => aa.PLeaveStageID).ToList(), "PLeaveStageID", "LeaveStageName", "A");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CommentView(VMESSPCommon vmESSPCommon)
+        {
+            VMLoggedUser LoggedInUser = Session["LoggedInUser"] as VMLoggedUser;
+            // Save Entry in Flow\
+            string Message = "";
+            switch (vmESSPCommon.FormName)
+            {
+                case "JobCard":
+                    if (vmESSPCommon.StageID == "A")
+                    {
+                        Message = JobCardESSPService.ApproveJobCard(vmESSPCommon, LoggedInUser, Message);
+                        ToasterMessages.Add("Job Card successfully Approved !");
+                        Session["ToasterMessages"] = ToasterMessages;
+                    }
+                    else
+                    {
+                        JobCardESSPService.RejectJobCard(vmESSPCommon, LoggedInUser);
+                        ToasterMessages.Add("Job card Rejected successfully !");
+                        Session["ToasterMessages"] = ToasterMessages;
+
+                    }
+                    break;
+                case "Leave":
+                    if (vmESSPCommon.StageID == "A")
+                    {
+                        LeaveApplicationESSPService.ApproveLeaveApplication(vmESSPCommon, LoggedInUser, Message);
+                        ToasterMessages.Add("Leave successfully Approved !");
+                        Session["ToasterMessages"] = ToasterMessages;
+                    }
+                    else
+                    {
+                        LeaveApplicationESSPService.RejectLeaveApplication(vmESSPCommon, LoggedInUser);
+                        ToasterMessages.Add("Leave Rejected successfully !");
+                        Session["ToasterMessages"] = ToasterMessages;
+                    }
+                    break;
+            }
+            return Json("OK", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ ESSPSERVICE/Generic/IEntityService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace ESSPSERVICE.Generic
+{
+    public class ServiceMessage
+    {
+        public List<string> ValidationMessage{ get; set; }
+    }
+    public interface IEntityService<T> where T : class
+    {
+        List<T> GetIndex();
+        List<T> GetIndexSpecific(Expression<Func<T, bool>> predicate);
+        T GetEdit(int id);
+        T GetDelete(int id);
+        ServiceMessage PostEdit(T obj);
+        ServiceMessage PostCreate(T obj);
+        ServiceMessage PostDelete(T obj);
+        int GetMaxID(Expression<Func<T, bool>> predicate);
+    }
+
+}
